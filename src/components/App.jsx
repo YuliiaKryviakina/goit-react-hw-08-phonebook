87b1ebb882f545ceeaec2f16b02b497776deeeaf
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-07-24 14:27:20 +0300

    update.App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,52 @@
-import ContactForm from "./contactForm/contactForm";
-import ContactList from "./contactList/contactList";
-import ContactFilter from "./contactFilter/contactFilter";
+import { Route, Routes } from 'react-router-dom';
+import { SignUp } from '../pages/SignUp/signUp';
+import { SharedLayout } from './sharedLayout/sharedLayout';
+import { Home } from '../pages/Home/home';
+import { Login } from '../pages/Login/login';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshUser } from 'redux/operations';
+import { Contacts } from 'pages/Contacts/contacts';
+import { PrivateRoute } from './PrivateRoute';
+import { PageNotFound } from 'pages/pageNotFound';
+import { RestrictedRoute } from './RestrictedRoute';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <ContactFilter />
-      <ContactList />
-    </div>
-  );
-}
+export const App = () => {
+  const dispatch = useDispatch();
+  const isrefresh = useSelector(state => state.auth.isRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  if (isrefresh === undefined) {
+    return <div>Liading</div>;
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+
+          <Route
+            path="/registration"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<SignUp />} />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+
+          <Route
+            path="/contacts"
+            element={<PrivateRoute redirectTo="/" component={<Contacts />} />}
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    );
+  }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { filterReducer } from "./filterSlice";
-import { contactsApi } from "./contactsApi";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { authReducer } from './slices';
+import { filterReducer } from './filterSlice';
+import { phonebookReducer } from './phoneBook';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: phonebookReducer,
     filter: filterReducer,
-    [contactsApi.reducerPath]: contactsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(contactsApi.middleware),
+
+  devTools: process.env.NODE_ENV === 'development',
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
