e0f4d9c88159e856809368706e6a0ef23df3aecb
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-07-24 14:06:43 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,5 @@
-export const selectFilterField = (state) => state.filter;
+export const selectFilterField = state => state.filter;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const getContacts = state => state.contacts.items;
